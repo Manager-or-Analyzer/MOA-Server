319729cdda8cfdc94d3aa79cd447bdda6eb58c06
dongyoung <chemicalatom@naver.com>
2016-01-10 22:58:09 +0900

    "mecab 외부 프로세스로 돌리기  "

--- MOA/src/main/java/kr/co/moa/keyword/anlyzer/morpheme/MorphemeAnalyzer.java
+++ MOA/src/main/java/kr/co/moa/keyword/anlyzer/morpheme/MorphemeAnalyzer.java
@@ -1,6 +1,14 @@
 package kr.co.moa.keyword.anlyzer.morpheme;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -35,40 +43,43 @@ public class MorphemeAnalyzer {
 	private static MorphemeAnalyzer instance;
 	private Map<String,String> TagsMap;
 	private Map<String,String> TexttagMap;
+	private String userid;
+	private String url;
 	
 	
 	 private static final String[] uselessTags = {
-	            "script", "noscript", "style", "meta", "link",
-	            "noframes", "nav", "aside", "hgroup", "header", "footer", "math",
-	            "button", "fieldset", "input", "keygen", "object", "output", "select", "textarea",
-	            "img", "br", "wbr", "embed", "hr","col", "colgroup", "command",
-	            "device", "area", "basefont", "bgsound", "menuitem", "param", "track","a",
-	            "i","aside","embed"
+	            "script", 	"noscript", "style", 	"meta", 	"link",
+	            "noframes", "nav", 		"aside", 	"hgroup", 	"header", 
+	            "footer", 	"math",		"button", 	"fieldset", "input", 
+	            "keygen", 	"object", 	"output", 	"select", 	"textarea",
+	            "img", 		"br", 		"wbr", 		"embed", 	"hr",
+	            "col", 		"colgroup", "command",	"device", 	"area", 
+	            "basefont", "bgsound", 	"menuitem", "param", 	"track",
+	            "a",		"i",		"aside"
 	 };
 	 //표는 버린다.ㅋ
 	 private static final String[] textTags = {
-			 "title", "p", "h1", "h2", "h3", "h4", "h5", "h6", "pre", "address",
-	         "ins", "textarea","blockquote", "dt","dd","span","b","font","strong"
+			 "title", 	"p", 		"h1", 			"h2", 		"h3", 
+			 "h4", 		"h5", 		"h6", 			"pre", 		"address",
+	         "ins", 	"textarea",	"blockquote", 	"dt",		"dd",
+	         "span",	"b",		"font",			"strong"
 	 };
 	
 	public static MorphemeAnalyzer getInstance(){
-		if(instance == null)
-			instance = new MorphemeAnalyzer();
-		return instance;
+		if(instance == null) 	instance = new MorphemeAnalyzer();
+		return 					instance;
 	}
 	
 	private MorphemeAnalyzer(){
 		TagsMap = new HashMap<String,String>();
-		for(String tag : uselessTags){
-			TagsMap.put(tag, null);
-		}
+		for(String tag : uselessTags){	TagsMap.put(tag, null);		}
 		TexttagMap = new HashMap<String,String>();
-		for(String tag : textTags){
-			TexttagMap.put(tag, null);
-		}
+		for(String tag : textTags)   {	TexttagMap.put(tag, null);	}
 	}
 	
 	public void parsingHTML(HtmlData html){
+		url = html.url;
+		userid = html.userid;
 		DebuggingLog debug = new DebuggingLog("Content");
 		HtmlParser hp = new HtmlParser();
 		HtmlParsedData hpd = new HtmlParsedData(html.userid, html.url, html.time);
@@ -82,35 +93,35 @@ public class MorphemeAnalyzer {
 			public Map<String,String> keywordList= domecab();
 		 */
 		String content = hp.makeCBT(html, TagsMap, TexttagMap, hpd).makeTopicTree();
-		  if(content.equals("") || content.trim().length() <120){
-	         System.out.println("lamda decrease");
-	         hp = new HtmlParser();
-	         hp.lamda = 0.05;
-	         content = hp.makeCBT(html, TagsMap, TexttagMap, hpd).makeTopicTree();
-	      }else
-	         System.out.println("length :" + content.length());
+		if(content.equals("") || content.trim().length() <120){
+			System.out.println("lamda decrease");
+	        hp = new HtmlParser();
+	        hp.lamda = 0.05;
+	        content = hp.makeCBT(html, TagsMap, TexttagMap, hpd).makeTopicTree();
+	    }else
+	        System.out.println("length :" + content.length());
 	      
-		  debug.write(content);
-		  debug.writeln();
-		  debug.writeln();
-	      System.out.println("title : " + hpd.title);
-	      System.out.println("content : "+content);
-	      System.out.println("decription : "+hpd.imrsrc);
-	      Map words_map = doMecab(content, "html");
-	      hpd.keywordList = words_map;
-	      
-	      debug.close();
-	      try {
-			DBManager.getInstnace().insertData("ParsedHtmlCollection", new Gson().toJson(hpd));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		
+		debug.write(content);
+		debug.writeln();
+	  	debug.writeln();
+	  	System.out.println("title : "  		+ hpd.title );
+	  	System.out.println("content : "		+ content   );
+	  	System.out.println("decription : "	+ hpd.imrsrc);
+//	    Map words_map = doMecab(content, "html");
+	  	Map words_map = doMecabProcess(content, "html");
+	  	hpd.keywordList = words_map;
+	     
+	  	debug.close();
+	  	try {
+	  		DBManager.getInstnace().insertData("ParsedHtmlCollection", new Gson().toJson(hpd));
+	  	} catch (Exception e) {
+	  		e.printStackTrace();
+	  	}
 	}
 	
 	public void parsingEvent(EventData eventData){
+		url = eventData.url;
+		userid = eventData.userid;
 		String content = eventData.data;
 		System.out.println(content);
 		
@@ -118,16 +129,21 @@ public class MorphemeAnalyzer {
 		//debug.write(content);
 		//debug.writeln();
 		//debug.writeln();
-		Map words_map = doMecab(content, "event");
-		EventParsedData epd = new EventParsedData(eventData.userid, eventData.url, eventData.time, words_map);
+		
+//		Map words_map = doMecab(content, "event");
+		Map words_map = doMecabProcess(content, "event");
+		EventParsedData epd = new EventParsedData(  eventData.userid, 
+													eventData.url, 
+												    eventData.time, 
+												    words_map);
 		
 		try {
 			DBManager.getInstnace().insertData("ParsedEventCollection", new Gson().toJson(epd));
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private Map doMecab(String content, String kind){
@@ -202,6 +218,112 @@ public class MorphemeAnalyzer {
 		return sorted_map;
 	}
 	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private Map doMecabProcess(String content, String kind){
+		DebuggingLog debug;
+		if(kind.equals("event"))
+			 debug = new DebuggingLog("KeywordsEvent");
+		else
+			 debug = new DebuggingLog("KeywordsHTml");
+		
+		String inputPath  = "/librarys/" + userid + "/";
+		String outputPath = "/librarys/" + userid + "/";
+		String inputFile  = "test.txt";
+		String outputFile = "test_out.txt";
+		
+		if(!makeInputFile(inputPath + inputFile, content)){
+			//inputfile 생성 오류처리
+		}
+		
+		List<String> arg = new ArrayList<String>();
+		arg.add("/librarys/mecab-ko/mecab");
+		arg.add("-d");
+		arg.add("/librarys/mecab-ko/dic/mecab-ko-dic");
+		arg.add(inputPath + inputFile);
+		arg.add("-o");
+		arg.add(outputPath + outputFile);
+		
+		try {
+			//System.out.println("start process------------------------");
+			ProcessBuilder mecab_builder = new ProcessBuilder(arg);
+			mecab_builder.redirectOutput(Redirect.INHERIT);	//에러와 출력을 표준스트림으로 출력시킴
+			mecab_builder.redirectError(Redirect.INHERIT);	//input-buffer overflow. The line is split. use -b #SIZE option.
+			Process mecab_process = mecab_builder.start();
+			mecab_process.waitFor();
+			//System.out.println("end process--------------------------");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//Stanford POS tagger
+        MaxentTagger tagger = null;
+	    try {
+	    	tagger = new MaxentTagger("C:\\Users\\dong\\Documents\\moa_gitt\\MOA\\left3words-wsj-0-18.tagger");
+	    } catch (ClassNotFoundException e1) {
+	    	e1.printStackTrace();
+	    } catch (IOException e1) {
+	    	e1.printStackTrace();
+	    }
+	    
+	    String word, type;
+		
+        Map<String, Integer> countingMap = new HashMap<String, Integer>();
+		ValueComparator bvc = new ValueComparator(countingMap);
+        TreeMap sorted_map = new TreeMap(bvc);
+        
+	    //System.out.println(tagger.tagString(content));
+        
+        List<WordTagPair> result = getVaildTag(outputPath + outputFile);
+        if(result == null){
+        	//mecab오류 예외처리
+        }
+        
+        for (WordTagPair term: result) {
+			type = term.tag;
+			
+			if(type.equals("SL")){					//if foreign laguage do Stanford POS tagger
+				word = tagger.tagString(term.word);
+				String[] temp = word.split("/");
+				if(temp[1].charAt(0) != 'N') 	continue;	//명사가 아닌 영어 무시  
+			}
+			word = term.word;
+			
+			if(countingMap.containsKey(word)) 	countingMap.put(word, countingMap.get(word) + 1);
+			else 								countingMap.put(word, 1);
+		}
+
+		sorted_map.putAll(countingMap);
+		Collection<String>  keys 	= sorted_map.keySet();
+		Collection<Integer> values	= sorted_map.values();
+		Iterator key_iter = keys.iterator();
+		Iterator val_iter = values.iterator();
+		int count = 10;
+		System.out.println("key\t count\t");
+		debug.write("key\t count\t");
+		while(key_iter.hasNext()){//count-- > 0){
+			String ikey = (String)  key_iter.next();
+			int ival 	= (Integer) val_iter.next();
+			System.out.println(ikey + "\t " + ival);
+			debug.write(ikey + "\t " + ival);
+			debug.writeln();
+		}
+		System.out.println("done");
+		// return 저장할 Json 형태
+		// Event 경우 
+		/*
+		"url" : "http://yeop9657.blog.me/220374891289",
+		"keword" : cnt,
+		"keword" : cnt,
+		"type" : "scroll",
+		 등등 등
+		
+		*/
+		debug.close();
+		return sorted_map;
+	}
+	
 	class ValueComparator implements Comparator {
 	    Map<String, Integer> base;
 
@@ -218,7 +340,67 @@ public class MorphemeAnalyzer {
 	        } // returning 0 would merge keys
 		}
 	}
-		
+
+	class WordTagPair {
+		String word;
+		String tag;
+	}
+	
+	private List getVaildTag (String filepath){
+		BufferedReader in;
+		ArrayList<WordTagPair> result = new ArrayList<WordTagPair>();
+		try {
+			in = new BufferedReader(new FileReader(filepath));
+			String str;
+
+		    while ((str = in.readLine()) != null) {
+		    	if(str.length() == 3 && str.equals("EOS")) continue;
+		    	String[] tok = str.split("\t");
+		    	if(tok[1].charAt(0) != 'N' 						&& 		//ignore NOT a noun
+		    			!tok[1].substring(0, 2).equals("SL") 	&&		//ignore NOT a foreign language
+		    			!tok[1].substring(0, 2).equals("SN")) continue;	//ignore NOT a number
+		    	WordTagPair entry = new WordTagPair();
+		    	entry.word = tok[0];
+		    	entry.tag = tok[1].split(",")[0];
+		    	result.add(entry);
+		    	
+		    }
+		    in.close();
+		    
+		    //for(WordTagPair p : result){
+	        //	System.out.println(p.word + "/" + p.tag);
+	        //}
+		    return result;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	    		
+		return null;
+	}
+	
+	private boolean makeInputFile(String path, String content){
+		String dirpath = "/librarys/" + userid;
+		dirpath.replace("/", "\\\\");
+		dirpath = "C:" + dirpath;
+		File dir = new File(dirpath);
+		if( !dir.exists() ){
+			dir.mkdir();
+		}
+					
+		path.replace("/", "\\\\");
+		path = "C:" + path;
+		BufferedWriter out;
+		try {
+			out = new BufferedWriter(new FileWriter(path));
+		    out.write(content);
+		    out.close();
+		    return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	    return false;
+	}
 }
 
 
